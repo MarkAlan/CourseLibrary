be6876c492984b071e05c1951e1ef2d800865c40
Mark Pownell <mapownell@gmail.com>
2020-10-15 22:57:27 -0400

    these are changes

---
+++ CourseLibrary.API/Models/CourseForMunipulationDto.cs
@@ -0,0 +1,20 @@
+﻿using CourseLibrary.API.ValidationAttributes;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CourseLibrary.API.Models
+{
+	[CourseTitleMustBeDifferentFromDescription(ErrorMessage = "Title must be different than the description")]
+	public abstract class CourseForMunipulationDto
+	{
+		[Required(ErrorMessage = "You should fill out a title.")]
+		[MaxLength(100, ErrorMessage = "The title should be less than 100 characters")]
+		public string Title { get; set; }
+
+		[MaxLength(1500, ErrorMessage = "The dscription should be less than 1500 characters")]
+		public virtual string Description { get; set; }
+	}
+}
